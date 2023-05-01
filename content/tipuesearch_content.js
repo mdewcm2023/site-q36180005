3752e5b29fb74ccc267a450e50369adc1e5f1111
site-q36180005 <onceonce105212@gmail.com>
2023-05-01 15:23:27 +0800

    w11-

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲 :.........................................', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲點我 \n 靜態點我', 'tags': '', 'url': 'About.html'}]};
